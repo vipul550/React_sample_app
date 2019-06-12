fc47fbd609d91a959ba66e27e401d901a06a9eae
vipul550 <hivipulhere203@gmail.com>
2019-06-12 21:48:02 +0530

    First commit
    Sample app

---
+++ precache-manifest.1b488647d40379f38bf2fb511a513aca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794579b699c6c2bc937b198096201445",
+    "url": "/Vipul_ashawa/react_sample_applications.git/index.html"
+  },
+  {
+    "revision": "315c248ed114a06bcb53",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/css/2.6ffce258.chunk.css"
+  },
+  {
+    "revision": "e2a045f087b4cc5405ad",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "315c248ed114a06bcb53",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/js/2.7b88e008.chunk.js"
+  },
+  {
+    "revision": "e2a045f087b4cc5405ad",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/js/main.93935c59.chunk.js"
+  },
+  {
+    "revision": "3201a5e1360d2d691d5b",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/js/runtime~main.86586315.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Vipul_ashawa/react_sample_applications.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
